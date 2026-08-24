73aa23bc2bfb8f88ac3521154cf2dec9b07e0685
Natalie Morgan <natalie@hexorra.com>
2026-08-24 18:32:13 +0100

    Solve Playback Speed

---
+++ week0/playback.py
@@ -0,0 +1,5 @@
+# Ask the user to type something, and store what they typed in a variable called "text"
+text = input("Type Something: ")
+
+# Convert the text to lowercase, then display it on screen
+print(text.replace(" ", "..."))
